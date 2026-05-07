08433ea2b538ce254a7ba0aa2acab1019504980c
Francesco <francesco.seddo@gmail.com>
2026-05-07 14:08:28 -0400

    Dynamic sizing: Landing About Us made more mobile friendly

--- frontend/src/landing/AboutUs.tsx
+++ frontend/src/landing/AboutUs.tsx
@@ -2,11 +2,12 @@ import { tw } from '../common/utils/tw';
 
 export const AboutUs = () => {
   return (
-    <section className='py-about-us flex w-full flex-col items-center gap-14 border-y-2'>
-      <header className='font-crimson text-about-us-header w-2/3 text-center font-medium'>
+    <section className='py-about-us flex w-full flex-col items-center gap-10 border-y-2 px-4'>
+      <header className='font-crimson text-about-us-header w-2/3 text-center leading-tight font-medium'>
         Curated for those with an eye for luxury, our bouquets are designed with{' '}
-        <span className='italic'>style and sophistication</span> in the heart of
-        New York and delivered nationwide.
+        <span className='italic'>style and sophistication</span> in the heart of{' '}
+        <span className='whitespace-nowrap'>New York</span> and delivered
+        nationwide.
       </header>
       <div
         className={tw(
